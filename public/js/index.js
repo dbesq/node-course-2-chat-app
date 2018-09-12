2a049de98f76aeb509db8d5322e8e6fc37595fec
dbEsq <dougmberman@gmail.com>
2018-09-12 16:34:33 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function (){
   console.log("Connected to server");
-
-  socket.emit('createMessage', {
-    to: 'jen@example.com',
-    from: 'notJen@example.com',
-    text: 'Hey. This is Andrew'
-  });
 });
 
 socket.on('disconnect', function (){
@@ -15,5 +9,5 @@ socket.on('disconnect', function (){
 });
 
 socket.on('newMessage', function (message){
-  console.log("New Message", message);
+  console.log("newMessage", message);
 });
--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -15,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
-  socket.emit('newMessage', {
-    from: 'mike@example.com',
-    text: 'Hey. What is going on.',
-    createdAt: 123
-  });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
